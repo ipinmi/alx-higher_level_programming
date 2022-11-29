d1098362e2ba14dd7e9e64ac4fc29f93cd11d5d8
ipinmi <ipinmiadebayo@gmail.com>
2022-11-29 23:47:23 +0000

    functions correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for letter in str:
         if ord(letter) in range(ord('a'), ord('z') + 1):
-            letter = chr(ord(letter)- 32)
-            print(f"{letter:s}", end="")
+            letter = chr(ord(letter) - 32)
+            print("{}".format(letter), end="")
     print()
