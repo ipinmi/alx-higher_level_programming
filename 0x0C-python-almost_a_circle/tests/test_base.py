2adfc411c2512cd494658e6230263e874162d2a4
ipinmi <ipinmiadebayo@gmail.com>
2023-01-16 21:19:35 +0000

    tests files

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines unittests for base.py.
+
+Unittest classes:
+    TestBase_instantiation - line 23
+    TestBase_to_json_string - line 110
+    TestBase_save_to_file - line 156
+    TestBase_from_json_string - line 234
+    TestBase_create - line 288
+    TestBase_load_from_file - line 340
+    TestBase_save_to_file_csv - line 406
+    TestBase_load_from_file_csv - line 484
+"""
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
