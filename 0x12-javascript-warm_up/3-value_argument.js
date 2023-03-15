4166df9ce1aea15337f4235f5466b0594b8b63a4
ipinmi <ipinmiadebayo@gmail.com>
2023-03-15 16:50:49 -0700

    semistandard fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(typeof ProcessingInstruction.argv[2] === 'undefined' ? 'No argument': ProcessingInstruction.argv[2]);
+console.log(typeof ProcessingInstruction.argv[2] === 'undefined' ? 'No argument' : ProcessingInstruction.argv[2]);
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let r = 0; r < size; r++) {
-        let row = '';
-        for (let col = 0; col < size; col++) row += 'X';
-        console.log(row);
-    }
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let col = 0; col < size; col++) row += 'X';
+    console.log(row);
+  }
 }
